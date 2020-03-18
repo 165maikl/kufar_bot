4e10b0ce90addaf6134079e11d32bfad8f028ee4
Mikhail Khlus <mikhail.hlus@gmail.com>
2020-03-18 17:58:48 +0300

    Удалена строка с выводом дебаг-информации
    Нет необходимости в данном коде

--- src/test/java/automationFramework/Kufar_Robot_docker.java
+++ src/test/java/automationFramework/Kufar_Robot_docker.java
@@ -139,7 +139,6 @@ public class Kufar_Robot_docker {
             System.out.println(firstName);
             System.out.println(currentDate);
             System.out.println();
-            System.out.println("--------------------");
 
 //            //кликаем по меню
 //            searchPoint.click();
